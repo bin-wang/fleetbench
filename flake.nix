274639a3bcb496adb0f9e9510aba056f5d2691cf
Bin Wang <biwa@google.com>
2026-05-01 23:24:30 +0000

    Softlink riscv64-unknown-linux-gnu-ld to ld/ld.bfd
    The linker installed in devShell is available as
    riscv64-unknown-linux-gnu-ld, but not as ld/ld.bfd, this will make
    bazel reach for /usr/bin/ld.bfd during build, which is the host version
    that doesn't understand riscv.
    
    I've added a linkerWrapper that creates a symbolic link from
    riscv64-unknown-linux-gnu-ld to ld/ld.bfd so bazel can use the correct
    linker during build.

--- flake.nix
+++ flake.nix
@@ -30,31 +30,48 @@
     {
       formatter.${system} = pkgs.nixfmt-tree;
       devShells.${system} = {
-        gcc = pkgs.mkShellNoCC {
-          packages = commonPackages ++ [
-            crossPkgs.buildPackages.gcc15
-            crossPkgs.buildPackages.binutils
-          ];
+        gcc =
+          let
+            linkerWrapper = pkgs.runCommand "linker-wrapper" { } ''
+              mkdir -p $out/bin
+              ln -s ${crossPkgs.buildPackages.gcc15}/bin/riscv64-unknown-linux-gnu-ld $out/bin/ld
+              ln -s ${crossPkgs.buildPackages.gcc15}/bin/riscv64-unknown-linux-gnu-ld $out/bin/ld.bfd
+            '';
+          in
+          pkgs.mkShellNoCC {
+            packages = commonPackages ++ [
+              crossPkgs.buildPackages.gcc15
+              linkerWrapper
+            ];
 
-          shellHook = commonShellHook + ''
-            export CC=riscv64-unknown-linux-gnu-gcc
-            export CXX=riscv64-unknown-linux-gnu-g++
-            echo "Loaded GCC 15 Cross-Compiler Environment"
-          '';
-        };
+            shellHook = commonShellHook + ''
+              export CC=riscv64-unknown-linux-gnu-gcc
+              export CXX=riscv64-unknown-linux-gnu-g++
+              echo "Loaded GCC 15 Cross-Compiler Environment"
+            '';
+          };
 
-        llvm = pkgs.mkShellNoCC {
-          packages = commonPackages ++ [
-            crossPkgs.buildPackages.llvmPackages_22.clang
-            crossPkgs.buildPackages.llvmPackages_22.bintools
-          ];
+        llvm =
+          let
+            linkerWrapper = pkgs.runCommand "linker-wrapper" { } ''
+              mkdir -p $out/bin
+              ln -s ${crossPkgs.buildPackages.llvmPackages_22.bintools}/bin/riscv64-unknown-linux-gnu-ld $out/bin/ld
+              ln -s ${crossPkgs.buildPackages.llvmPackages_22.bintools}/bin/riscv64-unknown-linux-gnu-ld $out/bin/ld.bfd
+            '';
+          in
+          pkgs.mkShellNoCC {
+            packages = commonPackages ++ [
+              crossPkgs.buildPackages.llvmPackages_22.clang
+              crossPkgs.buildPackages.llvmPackages_22.bintools
+              linkerWrapper
+            ];
 
-          shellHook = commonShellHook + ''
-            export CC=riscv64-unknown-linux-gnu-clang
-            export CXX=riscv64-unknown-linux-gnu-clang++
-            echo "Loaded Clang/LLVM 22 Cross-Compiler Environment"
-          '';
-        };
+            shellHook = commonShellHook + ''
+              export CC=riscv64-unknown-linux-gnu-clang
+              export CXX=riscv64-unknown-linux-gnu-clang++
+              echo "Loaded Clang/LLVM 22 Cross-Compiler Environment"
+            '';
+          };
       };
     };
 }
